9298071ad6a66d7c4682c4221c7c9bf02248c873
Queenore <atcheschev@yandex.ru>
2023-03-27 21:27:23 +0300

    fix MessagePage

--- src/test/java/MessagePage.java
+++ src/test/java/MessagePage.java
@@ -15,17 +15,17 @@ public class MessagePage {
 
     public MessagePage sendMessage(String message) {
         lastMessage = message;
-        $(By.xpath("//div[@class='js-lottie-observer']//div")).should(Condition.exist).sendKeys(message);
+        $(By.xpath("//div[@class='js-lottie-observer']//div")).should(Condition.visible).sendKeys(message);
         $(By.xpath("//div[@class='buttons __right']//msg-button[@title='Отправить']"))
-                .should(Condition.exist).click();
+                .should(Condition.visible).click();
         return this;
     }
 
     public void deleteLastSendMessage() {
         $(By.xpath("//span[@data-tsid='message_text']//span[contains(text(),'" + lastMessage + "')]"))
-                .should(Condition.exist).hover();
-        $(By.xpath("//msg-icon[@icon='menu']//*[name()='svg']")).should(Condition.exist).click();
-        $(By.xpath("//msg-tico[@icon='delete']//span[@class='tico_tx']")).should(Condition.exist).click();
-        $(By.xpath("//msg-button[@data-tsid='confirm-primary']")).should(Condition.exist).click();
+                .should(Condition.visible).hover();
+        $(By.xpath("//msg-icon[@icon='menu']//*[name()='svg']")).should(Condition.visible).click();
+        $(By.xpath("//msg-tico[@icon='delete']//span[@class='tico_tx']")).should(Condition.visible).click();
+        $(By.xpath("//msg-button[@data-tsid='confirm-primary']")).should(Condition.visible).click();
     }
 }
